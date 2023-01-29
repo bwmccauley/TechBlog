616e397547142189a09d3b72c4bdfde67eef27ad
bwmccauley <brandonmccauley@Brandons-MacBook-Pro.local>
2023-01-29 16:56:10 -0500

    changed credentials

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,16 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306
+//     });
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize("ddp1ep8eh1r28f", "bdzodrrotvdwuo", "4c07fba9ce8a07850b3ed1920adc0cab38e77016ac4ba42e22ede512a8422055", {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
